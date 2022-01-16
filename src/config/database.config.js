59b09802699a6a5ad504c581a16f891c19bbe65d
amankrokx <amankumar.spj410@gmail.com>
2022-01-16 20:41:30 +0530

    lol

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,29 +1,33 @@
 // ************************************abhishek************************************
 
-
-// this page is created to connect the databse 
-const mysql=require('mysql');
+// this page is created to connect the databse
+const mysql = require("mysql");
 
 // mysql connection
 // connection requires hostname,user (you can change based on your local databse)
 // password (again depend on your MySql workbench or configuration)
 // at the end we need to specify the databse in which you are working on
-const dbConnection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password: 'root',
-    database:'placement'
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "placement",
+});
+
+// to hadle the errors
+db.connect(function (error) {
+  if (error) throw error;
+  console.log("Database Connection is established !");
+  // test database connection
+  db.query("SELECT * FROM student", (error, results, fields) => {
+    if (error) throw error;
+    console.log(fields, results);
+  });
 });
 
 
-// to hadle the errors
-dbConnection.connect(function(error){
-    if(error) throw error;
-    console.log('Database Connection is established !');
-})
 
 // to export the database information
-module.exports=dbConnection;
-
+module.exports = db;
 
 // ***********************************************************************
