3facc1f07cef3f8177e8057beb650be610fb9d72
Tishya Tripathi <tishyatripathi@outlook.com>
2022-03-19 13:12:51 +0530

    updated admin dashboard

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import StudentClass12Records from "./components/Student_Dashboard/StudentRecords
 import StudentBacklogsRecords from "./components/Student_Dashboard/StudentRecords/BacklogsRecords";
 import StudentDiplomaRecords from "./components/Student_Dashboard/StudentRecords/DiplomaRecords";
 import StudentGraduationRecords from "./components/Student_Dashboard/StudentRecords/GraduationRecords";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import Home from "./components/Home";
 import { AdminSignIn } from "./components/Admin/AdminSignIn"
 import StudentDashboard from "./components/Student_Dashboard/StudentDashboard";
-import StudentDashboardRecords from "./components/Student_Dashboard/StudentDashboardRecords"
+import StudentDashboardRecords from "./components/Student_Dashboard/StudentDashboardRecords";
 import StudentDashboardProfile from "./components/Student_Dashboard/StudentDashboardProfile"
 import StudentDashboardNotifications from "./components/Student_Dashboard/StudentDashboardNotifications"
-
 import StudentDashboardAnalytics from "./components/Student_Dashboard/StudentDashboardAnalytics"
 import StudentDashboardEvents from "./components/Student_Dashboard/StudentDashboardEvents"
 import AdminDashboard from "./components/Admin/AdminDashboard"
-
-import StudentDashboardAnalytics from "./components/Student_Dashboard/StudentDashboardAnalytics";
+import AdminDashboardNotifications from "./components/Admin/AdminDashboardNotifications"
+import AdminDashboardStudents from "./components/Admin/AdminDashboardStudents"
+import AdminDashboardEvents from "./components/Admin/AdminDashboardEvents"
 
 
 
@@ -47,21 +47,17 @@ function App() {
           <Route path="/signed_in/student_dashboard/records/diploma" exact element={<StudentDiplomaRecords />} />
           <Route path="/signed_in/student_dashboard/records/backlogs" exact element={<StudentBacklogsRecords />} />   
 
-
           <Route path="/signed_in/student_dashboard/events" exact element={<StudentDashboardEvents />} />
           
-=======
-          {/* <Route path="/signed_in/student_dashboard/events" exact element={<StudentDashboardEvents />} /> */}
-          {/* <Route path="/signed_in/student_dashboard/analytics" exact element={<StudentDashboardAnalytics />} /> */}
-          <Route path="/signed_in/student_dashboard/records" exact element={<StudentDashboardRecords />} />
-
           <Route path="/signed_in/student_dashboard/analytics" exact element={<StudentDashboardAnalytics />} />
 
           
           {/* Admin Dashboard */}
           <Route path="/admin/signin" exact element={<AdminSignIn />} />
           <Route path="/admin/signed_in/dashboard" exact element={<AdminDashboard />} /> 
-
+          <Route path="/admin/signed_in/dashboard/notifications" exact element={<AdminDashboardNotifications />} />
+          <Route path="/admin/signed_in/dashboard/students" exact element={<AdminDashboardStudents />} />
+          <Route path="/admin/signed_in/dashboard/events" exact element={<AdminDashboardEvents />} />
 
         </Routes>
       </div>
---
+++ src/components/Admin/AdminDashboardNotifications.js
@@ -0,0 +1,210 @@
+import * as React from "react";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Link from "@mui/material/Link";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { mainListItems, secondaryListItems } from "./listItems";
+import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://www.rnsit.ac.in/">
+        RNSIT
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+const mdTheme = createTheme();
+
+// --------------------------------------------------------------------------------------------
+function AdminDashboardContentNotifications() {
+  const [open, setOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+
+        <AppBar position="absolute" open={open} elevation={0}>
+          <Toolbar
+            sx={{
+              background: "#021B38",
+              pr: "24px",
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+                Dashboard <Typography variant="p" color="#ffeb3b">
+                Notifications
+              </Typography>
+
+              {/* ------Conditionally render these Badges for TPC, DFPC and Admin------  */}
+
+              <Badge badgeContent={"Admin"} color="success" sx={{px:3,}} ></Badge>
+              {/* <Badge badgeContent={"TPC"} color="secondary" sx={{px:3,}}></Badge> */}
+              {/* <Badge badgeContent={"DFPC"} color="error" sx={{px:3,}}></Badge> */}
+            
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List>{mainListItems}</List>
+          <Divider />
+          <List>{secondaryListItems}</List>
+        </Drawer>
+
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            minHeight: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    minHeight: 280,
+                  }}
+                ></Paper>
+              </Grid>
+            </Grid>
+
+            {/* <Grid item xs={12}>
+              <Paper
+                sx={{
+                  mt: 5,
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  minHeight: 280,
+                }}
+              ></Paper>
+            </Grid> */}
+
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default function AdminDashboardNotifications() {
+  return <AdminDashboardContentNotifications />;
+}
