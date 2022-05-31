68eb919f32084abad248298bd05f20e3f7abf712
Aman kumar <37014828+amankrokx@users.noreply.github.com>
2022-05-31 23:27:36 +0530

    Update listItems.js

--- src/components/Student_Dashboard/listItems.js
+++ src/components/Student_Dashboard/listItems.js
@@ -13,8 +13,22 @@ import { fet } from "../modules/fet"
 
 import { Link } from "react-router-dom";
 
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 const logoutFun = async() => {
   fet("/logout","GET").then(response=> console.log(response));
+  localStorage.clear();
+  sessionStorage.clear();
+  deleteAllCookies();
 };
 
 export const mainListItems = (
